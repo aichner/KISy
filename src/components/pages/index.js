13f87fc7937a6f7c6bdeb18ad08941e43ecc6f11
Christian Aichner <contact@aichner-christian.com>
2020-04-14 22:02:38 +0200

    Improve code quality
    The code quality has been improved.

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -4,13 +4,9 @@ import ProfilePage from "./ProfilePage";
 import LoginPage from "./LoginPage";
 import MessagePage from "./MessagePage";
 
-export {
-    ProfilePage,
-    LoginPage,
-    MessagePage,
-};
+export { ProfilePage, LoginPage, MessagePage };
 
-/** 
+/**
  * SPDX-License-Identifier: (EUPL-1.2)
  * Copyright © 2019 Christian Aichner
  */
