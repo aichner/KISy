8019b4146593b73f8326334b8eed48d2d392f56a
Christian Aichner <contact@aichner-christian.com>
2020-04-15 09:58:02 +0200

    Improve code quality
    The code quality has been improved as requested
    by @kleberbaum.

--- src/components/molecules/charts/ResultChart/index.jsx
+++ src/components/molecules/charts/ResultChart/index.jsx
@@ -6,14 +6,14 @@ import React from "react";
 // Chart
 import { Radar } from "react-chartjs-2";
 
-class ResultChart extends React.Component{
+class ResultChart extends React.Component {
   state = {
     dataRadarOptions: {
       responsive: true,
       elements: {
         line: {
-          tension: 0.2
-        }
+          tension: 0.2,
+        },
       },
       legend: {
         display: false,
@@ -21,104 +21,107 @@ class ResultChart extends React.Component{
       scale: {
         pointLabels: {
           fontSize: 16,
-          fontFamily: "Roboto"
+          fontFamily: "Roboto",
         },
         ticks: {
           beginAtZero: true,
           display: false,
           max: 100,
           min: 0,
-          stepSize: 20
-        }
+          stepSize: 20,
+        },
       },
       scales: {
-        yAxes: [{
-          gridLines: {
-            display: false,
-            drawBorder: false
+        yAxes: [
+          {
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              display: false,
+            },
           },
-          ticks: {
-            display: false
-          }
-        }],
-        xAxes: [{
-          gridLines: {
-            display: false,
-            drawBorder: false
+        ],
+        xAxes: [
+          {
+            gridLines: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              beginAtZero: true,
+              display: false,
+            },
           },
-          ticks: {
-            beginAtZero: true,
-            display: false,
-          }
-        }]
-      }
+        ],
+      },
     },
-    dataRadarPlugins: [{
+    dataRadarPlugins: [
+      {
         beforeInit: function (chart) {
-        chart.data.labels.forEach(function (e, i, a) {
+          chart.data.labels.forEach(function (e, i, a) {
             if (/\n/.test(e)) {
-            a[i] = e.split(/\n/)
+              a[i] = e.split(/\n/);
             }
-        })
-        }
-    }]
-  }
+          });
+        },
+      },
+    ],
+  };
 
   getChart = () => {
-    if(this.props.data){
+    if (this.props.data) {
       let labels, data;
       labels = [];
       data = [];
 
       // Get value and label
       Object.keys(this.props.data).map((keyName, i) => {
-          console.log(keyName, this.props.data[keyName]);
-          labels.push(this.props.data[keyName].name);
-          data.push(this.props.data[keyName].value);
+        console.log(keyName, this.props.data[keyName]);
+        labels.push(this.props.data[keyName].name);
+        data.push(this.props.data[keyName].value);
       });
 
       // Set data radar chart
       this.setState({
         dataRadar: {
           labels,
-          datasets: [{
-            label: "# of Votes",
-            data,
-            backgroundColor: [
-              "rgba(246, 26, 66, 0.2)",
-            ],
-            borderColor: [
-              "rgba(246, 26, 66, 1)",
-            ],
-            borderWidth: 1
-          }]
+          datasets: [
+            {
+              label: "# of Votes",
+              data,
+              backgroundColor: ["rgba(246, 26, 66, 0.2)"],
+              borderColor: ["rgba(246, 26, 66, 1)"],
+              borderWidth: 1,
+            },
+          ],
         },
       });
     }
-  }
+  };
 
-  render(){
+  render() {
     console.log(this.props, this.state);
-    
-    if(this.state.dataRadar){
-      return(
+
+    if (this.state.dataRadar) {
+      return (
         <Radar
-        data={this.state.dataRadar}
-        options={this.state.dataRadarOptions}
-        plugins={this.state.dataRadarPlugins}
+          data={this.state.dataRadar}
+          options={this.state.dataRadarOptions}
+          plugins={this.state.dataRadarPlugins}
         />
       );
     } else {
       this.getChart();
       return null;
     }
-    
   }
 }
 
 export default ResultChart;
 
-/** 
+/**
  * SPDX-License-Identifier: (EUPL-1.2)
  * Copyright © 2020 Christian Aichner
  */
