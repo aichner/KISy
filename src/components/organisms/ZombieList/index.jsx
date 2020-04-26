c62f06591536aa739c3075247376acf441affe86
Aichnerc <contact@aichner-christian.com>
2020-04-27 01:05:08 +0200

    Improve code quality
    A console.log has been removed.

--- src/components/organisms/ZombieList/index.jsx
+++ src/components/organisms/ZombieList/index.jsx
@@ -252,7 +252,6 @@ class ZombieList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  console.log(state);
   return {
     auth: state.firebase.auth,
     profile: state.firebase.profile,
