23925734289bab8053ef9d8eb4e0557b7cc6ad2f
Christian Aichner <contact@aichner-christian.com>
2020-04-29 18:27:46 +0200

    Improve services
    The service page has been improved.

--- src/components/molecules/Services/index.jsx
+++ src/components/molecules/Services/index.jsx
@@ -2,6 +2,10 @@
 // Contains all the functionality necessary to define React components
 import React from "react";
 
+//> Additional
+// Axios
+import axios from "axios";
+
 //> MDB
 // "Material Design for Bootstrap" is a great UI design framework
 import {
@@ -22,29 +26,31 @@ import {
   MDBNavItem,
   MDBTabContent,
   MDBTabPane,
+  MDBAlert,
   MDBProgress,
 } from "mdbreact";
 
 //> Tabs
 const tabs = [
-  { name: "Übersicht", color: "blue", icon: "list" },
   { name: "Meine Website", color: "orange", icon: "desktop" },
   {
     name: "Mein Social Media",
     color: "cyan",
-    icon: "paint-brush",
+    icon: "comments",
   },
   { name: "Mein Image", color: "green", icon: "award" },
   { name: "Meine Präsenzen", color: "deep-purple", icon: "bullhorn" },
-  {
-    name: "Meine Rechnungen",
-    color: "blue",
-    icon: "file-invoice-dollar",
-  },
 ];
 
 class Services extends React.Component {
   state = { activeItem: 0 };
+
+  componentDidMount = () => {
+    //this.getAnalytics();
+    if (this.props.profile && this.props.profile.analysis) {
+      this.getDetails();
+    }
+  };
 
   toggleInnerPills = (tab) => (e) => {
     e.preventDefault();
@@ -54,6 +60,113 @@ class Services extends React.Component {
         activeItem: tab,
       });
     }
+  };
+
+  getDetails = () => {
+    const rawData = this.props.profile.analysis[
+      this.props.profile.analysis.length - 1
+    ].raw_data;
+    const webResults = Object.keys(rawData).map((key, i) => {
+      switch (key) {
+        case "web_analysis":
+          return {
+            value: rawData[key],
+            name: "Analytics Einbindungen",
+          };
+          break;
+        case "web_design":
+          return {
+            value: rawData[key],
+            name: "Design und Useability",
+          };
+          break;
+        case "web_contact":
+          return {
+            value: rawData[key],
+            name: "Kontaktmöglichkeiten",
+          };
+          break;
+        case "web_legal":
+          return {
+            value: rawData[key],
+            name: "DSGVO, Impressumpflicht und E-Commerce",
+          };
+          break;
+        case "web_picture_quality":
+          return {
+            value: rawData[key],
+            name: "Qualität des Inhalts",
+          };
+          break;
+        case "web_responsive":
+          return {
+            value: rawData[key],
+            name: "Mobile Ansicht",
+          };
+          break;
+        case "web_wording":
+          return {
+            value: rawData[key],
+            name: "Wording",
+          };
+          break;
+        case "seo_https":
+          return {
+            value: rawData[key],
+            name: "Sicherheit",
+          };
+          break;
+        case "seo_meta":
+          return {
+            value: rawData[key],
+            name: "Meta Tags",
+          };
+          break;
+        case "seo_traceability":
+          return {
+            value: rawData[key],
+            name: "Auffindbarkeit",
+          };
+          break;
+        case "seo_domain":
+          return {
+            value: rawData[key],
+            name: "Deine Domain",
+          };
+          break;
+        default:
+          return null;
+      }
+    });
+
+    this.setState({
+      webResults: webResults.filter((x) => x),
+    });
+  };
+
+  getAnalytics = () => {
+    /*const url =
+      "https://api.cloudflare.com/client/v4/zones/cd7d0123e3012345da9420df9514dad0";
+
+    axios
+      .get(url, {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_CLOUDFLARE}`,
+          "Content-Type": "application/json",
+        },
+      })
+      .then(
+        (response) => {
+          const res = response.data;
+
+          console.log(res);
+        },
+        (error) => {
+          const status = error;
+
+          console.log(status);
+        }
+      );*/
   };
 
   render() {
@@ -84,13 +197,225 @@ class Services extends React.Component {
         <MDBCol md="8">
           <MDBTabContent activeItem={this.state.activeItem}>
             <MDBTabPane tabId={0}>
-              <h5>Panel 1</h5>
+              {profile.us && (
+                <>
+                  {profile.us.web ? (
+                    <MDBAlert color="info">
+                      Deine Website wird von uns gewartet.
+                    </MDBAlert>
+                  ) : (
+                    <MDBAlert color="warning">
+                      Deine Website wurde nicht von uns erstellt.
+                    </MDBAlert>
+                  )}
+                  {profile.us.web ? (
+                    <p className="text-muted">Analytics in Arbeit</p>
+                  ) : (
+                    <>
+                      <span className="mb-0">Deine Website Analyse</span>
+                      <MDBProgress
+                        value={
+                          profile.analysis[profile.analysis.length - 1].results
+                            .web.value
+                        }
+                        className="mb-0"
+                      />
+                      <small className="d-block text-muted mb-2">
+                        {Math.round(
+                          (profile.analysis[profile.analysis.length - 1].results
+                            .web.value +
+                            Number.EPSILON) *
+                            100
+                        ) / 100}{" "}
+                        / 100
+                      </small>
+                      <div className="my-2">
+                        {this.state.webResults &&
+                          this.state.webResults.map((item, i) => {
+                            return (
+                              <p className="feature" key={i}>
+                                {item.value <= 40 && (
+                                  <MDBIcon
+                                    icon="times-circle"
+                                    className="red-text"
+                                  />
+                                )}
+                                {item.value > 40 && item.value < 80 && (
+                                  <MDBIcon
+                                    icon="circle"
+                                    className="orange-text"
+                                  />
+                                )}
+                                {item.value >= 80 && (
+                                  <MDBIcon
+                                    icon="check-circle"
+                                    className="green-text"
+                                  />
+                                )}
+                                {item.name}
+                              </p>
+                            );
+                          })}
+                      </div>
+                      <p className="mt-4 mb-1">
+                        Du hast eine Frage dazu oder willst Deine Homepage
+                        verbessern?
+                      </p>
+                      <a href="mailto:info@aichner-christian.com">
+                        <MDBBtn color="agency-red">Kontakt</MDBBtn>
+                      </a>
+                    </>
+                  )}
+                </>
+              )}
+              {/*<iframe
+                sandbox="allow-same-origin allow-forms allow-scripts"
+                src="https://www.aichner-christian.com"
+                width="320"
+                height="320"
+                style={{ height: "500px" }}
+              ></iframe>*/}
             </MDBTabPane>
             <MDBTabPane tabId={1}>
-              <h5>Panel 2</h5>
+              {profile.us && (
+                <>
+                  {profile.us.social ? (
+                    <MDBAlert color="info">
+                      Dein Social Media wird von uns verwaltet.
+                    </MDBAlert>
+                  ) : (
+                    <MDBAlert color="warning">
+                      Dein Social Media wird nicht von uns verwaltet.
+                    </MDBAlert>
+                  )}
+                  <span className="mb-0">Deine Social Media Analyse</span>
+                  <MDBProgress
+                    value={
+                      (profile.analysis[profile.analysis.length - 1].results
+                        .facebook.value +
+                        profile.analysis[profile.analysis.length - 1].results
+                          .instagram.value) /
+                      2
+                    }
+                    className="mb-0"
+                  />
+                  <small className="d-block text-muted mb-2">
+                    {Math.round(
+                      ((profile.analysis[profile.analysis.length - 1].results
+                        .facebook.value +
+                        profile.analysis[profile.analysis.length - 1].results
+                          .instagram.value) /
+                        2 +
+                        Number.EPSILON) *
+                        100
+                    ) / 100}{" "}
+                    / 100
+                  </small>
+                  {profile.analysis[profile.analysis.length - 1].raw_data
+                    .has_facebook ? (
+                    <p className="feature">
+                      <MDBIcon icon="check-circle" className="green-text" />
+                      Du bist auf Facebook aktiv
+                    </p>
+                  ) : (
+                    <p className="feature">
+                      <MDBIcon icon="times-circle" className="red-text" />
+                      Du bist auf Facebook nicht aktiv
+                    </p>
+                  )}
+                  {profile.analysis[profile.analysis.length - 1].raw_data
+                    .has_instagram ? (
+                    <p className="feature">
+                      <MDBIcon icon="check-circle" className="green-text" />
+                      Du bist auf Instagram aktiv
+                    </p>
+                  ) : (
+                    <p className="feature">
+                      <MDBIcon icon="times-circle" className="red-text" />
+                      Du bist auf Instagram nicht aktiv
+                    </p>
+                  )}
+                  <p className="text-muted">
+                    <small>
+                      Wir erstellen und verwalten Social Media Profile,
+                      erschaffen Content und setzen Kampagnen um.
+                    </small>
+                  </p>
+                </>
+              )}
             </MDBTabPane>
             <MDBTabPane tabId={2}>
-              <h5>Panel 3</h5>
+              {profile.us && (
+                <>
+                  {profile.us.image ? (
+                    <MDBAlert color="info">
+                      Dein Image wird von uns verwaltet.
+                    </MDBAlert>
+                  ) : (
+                    <MDBAlert color="warning">
+                      Dein Image wird nicht von uns verwaltet.
+                    </MDBAlert>
+                  )}
+                  <p className="text-muted">
+                    <small>
+                      Zu Deinem Image zählt Dein Logo, Dein Auftreten auf
+                      Plattformen, sämtliche Imagefilme, Deine Glaubwürdigkeit,
+                      Fotos, Deine Story und Kontinuität.
+                    </small>
+                  </p>
+                </>
+              )}
+            </MDBTabPane>
+            <MDBTabPane tabId={3}>
+              {profile.us && (
+                <>
+                  {profile.us.presences ? (
+                    <MDBAlert color="info">
+                      Deine Präsenzen werden von uns verwaltet.
+                    </MDBAlert>
+                  ) : (
+                    <MDBAlert color="warning">
+                      Deine Präsenzen werden nicht von uns verwaltet.
+                    </MDBAlert>
+                  )}
+                  {profile.analysis[profile.analysis.length - 1].raw_data
+                    .has_gsuite ? (
+                    <p className="feature">
+                      <MDBIcon icon="check-circle" className="green-text" />
+                      Du besitzt Google Business
+                    </p>
+                  ) : (
+                    <p className="feature">
+                      <MDBIcon icon="times-circle" className="red-text" />
+                      Du besitzt kein Google Business
+                    </p>
+                  )}
+                  {profile.analysis[profile.analysis.length - 1].raw_data
+                    .has_facebook ? (
+                    <p className="feature">
+                      <MDBIcon icon="check-circle" className="green-text" />
+                      Du besitzt Facebook
+                    </p>
+                  ) : (
+                    <p className="feature">
+                      <MDBIcon icon="times-circle" className="red-text" />
+                      Du besitzt kein Facebook
+                    </p>
+                  )}
+                  {profile.analysis[profile.analysis.length - 1].raw_data
+                    .has_instagram ? (
+                    <p className="feature">
+                      <MDBIcon icon="check-circle" className="green-text" />
+                      Du besitzt Instagram
+                    </p>
+                  ) : (
+                    <p className="feature">
+                      <MDBIcon icon="times-circle" className="red-text" />
+                      Du besitzt kein Instagram
+                    </p>
+                  )}
+                </>
+              )}
             </MDBTabPane>
           </MDBTabContent>
         </MDBCol>
